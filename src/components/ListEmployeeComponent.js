66e7c03d51d03a8a4b9d385e2fdb6dc4675c984c
ValerioMarsala <marsalavalerio@gmail.com>
2022-06-18 13:38:56 +0200

    first commit

---
+++ src/components/ListEmployeeComponent.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import EmployeeService from "../services/EmployeeService";
+
+const ListEmployeeComponent = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    EmployeeService.getAllEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="text-center">List Employees</h2>
+      <table className="table table-bordered table-striped">
+        <thead>
+          <th>Employee Id</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.emailId}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListEmployeeComponent;
